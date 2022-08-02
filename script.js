f86bfbd56b453ad4f518d5f16addfc65a03c9e03
Julie Desová <juliedesova@seznam.cz>
2022-08-02 09:30:31 +0200

    hotovo

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("JavaScript funguje!");
+//console.log("JavaScript funguje!");
+
+//console.log( Math.floor( Math.random()*100 ));
+//
+//let mojeBarvaPodlahy = prompt( "Zadej barvu tvé podlahy." );
+//
+//console.log( mojeBarvaPodlahy );
+
+
+
+//let rokNarozeni = prompt( "Zadejte rok svého narozeni");
+//
+//let dnesniDatum = new Date();
+//
+//let vek = dnesniDatum.getFullYear() - rokNarozeni;
+//
+//console.log( "Uživatel má " + vek + "let.");
+
+
+function pozdrav(){
+    console.log("Ahoj!");
+}
